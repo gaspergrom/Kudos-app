8edba84fd7edb54ee3e5d3f7e91279c950954db5
ziga <ziga.likar1@gmail.com>
2018-10-20 22:01:02 +0200

    Adding departments to companies

--- api/routes/database-models/companies.js
+++ api/routes/database-models/companies.js
@@ -1,9 +1,47 @@
 const
-    employeeModel = require('../../models/employee'),
+    CompanyModel = require('../../models/company'),
+    EmployeeModel = require('../../models/employee'),
+    DepartmentModel = require('../../models/department'),
     name = 'company',
     pluralName = 'companies',
     BaseRouter = require('../base-router'),
     router = new BaseRouter(name, pluralName, ['departments']);
+
+router.router.post('/' + pluralName + '/:id/departments', async (req, res) => {
+    if (req && req.params && req.body && req.params.id) {
+        const id = req.params.id;
+        let company = await CompanyModel.findById(id);
+        
+        if (company) {
+            DepartmentModel.create(req.body, (err, added) => {
+                if (err) {
+                    console.error('Failed to create a department: ' + err);
+                    res.json();
+                }
+                else {
+                    company.departments.push(added);
+                    CompanyModel.findByIdAndUpdate(id, company, (err, original) => {
+                        if (err)
+                            console.error('Failed to update company ID \'' + id + '\' with department: ' + err);
+                        
+                        res.json(company);
+                    });
+                }
+            });
+        }
+        else
+            res.json();
+    
+        data.exec((err, found) => {
+            if (err)
+                console.error('Error when fetching \'' + this.tag + '\' ID ' + id);
+    
+            res.json(found);
+        });
+    }
+    else
+        res.json();
+});
 
 /**
  * Returns an array of all employees of the specified company
@@ -12,7 +50,7 @@ router.router.get('/' + pluralName + '/:id/employees', (req, res) => {
     if (req && req.params && req.params.id) {
         const id = req.params.id;
 
-        employeeModel
+        EmployeeModel
             .find({ company: id })
             .populate('departments')
             .exec((err, filtered) => {
