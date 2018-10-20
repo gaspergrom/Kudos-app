3ebb0e6563db54ca7930fb9f9e26858d0c0e3670
ziga <ziga.likar1@gmail.com>
2018-10-20 14:38:56 +0200

    Redeem kudos route

--- api/routes/other/redeem-kudos.js
+++ api/routes/other/redeem-kudos.js
@@ -4,23 +4,29 @@ const
     { Router } = require('express'),
     router = Router();
 
-router.post('/redeem-kudos', (req, res) => {
+router.post('/redeem-kudos', async (req, res) => {
+    bool 
     if (req && req.body) {
         const option = req.body.redeemOption;
-        const employee = req.body.employee; // MongoDB employee
+        const employeeId = req.body.employeeId; // MongoDB employee
 
-        console.log('option ' + option + ' for employee ' + employee)
-        if (employee && option < conf.redeemOptions.length) {
+        console.log('option ' + option + ' for employee ' + employeeId)
+        if (employeeId && option < conf.redeemOptions.length) {
             const redeemOption = conf.redeemOptions[option];
+            const employee = await employeeModel.findById(employeeId).exec();
 
-            if (redeemOption.amount <= employee.availableKudos) {
-                employee.availableKudos -= redeemOption.amount;
-                employeeModel.findByIdAndUpdate(employee['_id'], employee, { upsert: false }).exec((err, original) => {
-                    if (err)
-                        console.error('Error when patching \'' + this.tag + '\' ID ' + id + ' - problem when searching.');
-                    
-                    res.json(original);
-                });
+            if (employee) {
+                if (redeemOption.amount <= employee.availableKudos) {
+                    employee.availableKudos -= redeemOption.amount;
+                    employeeModel.findByIdAndUpdate(employeeId, employee, { upsert: false }).exec((err, original) => {
+                        if (err)
+                            console.error('Error when patching \'' + this.tag + '\' ID ' + id + ' - problem when searching.');
+                        
+                        res.json(original);
+                    });
+                }
+                else
+                    res.json();
             }
             else
                 res.json();
