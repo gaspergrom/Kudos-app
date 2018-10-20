9f1caf5a04dad7555fe528011bca4d75d8c233f7
ziga <ziga.likar1@gmail.com>
2018-10-20 13:06:09 +0200

    Config for Heroku

--- components/app/header.vue
+++ components/app/header.vue
@@ -6,7 +6,7 @@
 
         <h2 class="c-navbar__title"></h2>
         <div class="pr10">
-            <a href="https://slack.com/oauth/authorize?scope=commands users:read team:read&client_id=373180051559.455692683652&redirect_uri=http://localhost:3000/slack&state=workspace"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+            <a href="https://slack.com/oauth/authorize?scope=commands users:read team:read&client_id=373180051559.455692683652&redirect_uri=https://kudos-app-celtra.herokuapp.com/slack&state=workspace"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
 
         </div>
         <app-header-user></app-header-user>
--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,7 @@
             <div class="col-md-6">
                 <h2 class="pb20 text-center">Login to Kudos</h2>
                 <div class="flex flex--center">
-                    <a href="https://slack.com/oauth/authorize?scope=identity.basic identity.team identity.avatar&client_id=373180051559.455692683652&redirect_uri=http://localhost:3000/slack&state=user"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
+                    <a href="https://slack.com/oauth/authorize?scope=identity.basic identity.team identity.avatar&client_id=373180051559.455692683652&redirect_uri=https://kudos-app-celtra.herokuapp.com/slack&state=user"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
                 </div>
             </div>
         </div>
