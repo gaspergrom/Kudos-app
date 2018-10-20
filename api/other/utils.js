2ddd02bbdebcbe9533e5ad63c3b4b779696086d8
ziga <ziga.likar1@gmail.com>
2018-10-20 22:57:56 +0200

    Bf

--- api/other/utils.js
+++ api/other/utils.js
@@ -55,10 +55,10 @@ module.exports = {
      */
     processSlackWorkspace: async function (usersList, teamInfo) {
         if (usersList && teamInfo) {
-            let companyQuery = companyRouter.baseClass.findOne({ slackId: teamInfo.id })
-                .populate('departments');
-            
-            let company = await companyQuery.exec();
+            let company = await companyRouter.baseClass
+                .findOne({ slackId: teamInfo.id })
+                .populate('departments')
+                .exec();
             
             let employees = [];
     
@@ -67,11 +67,11 @@ module.exports = {
     
             for (let i in usersList) {
                 const user = usersList[i];
-                let employeeQuery = employeeRouter.baseClass.findOne({ slackId: user.id })
+                let employee = await employeeRouter.baseClass
+                    .findOne({ slackId: user.id })
                     .populate('company')
-                    .populate('departments');
-                
-                let employee = await employeeQuery.exec();
+                    .populate('departments')
+                    .exec();
     
                 if (employee == null || employee == undefined)
                     employee = await this.addEmployee(user, company._id);
