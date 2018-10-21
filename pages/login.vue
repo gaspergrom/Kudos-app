59295fb3e5a26d032f1a9dfb5c1a81199256d4f8
Gašper Grom <gasper.grom@gmail.com>
2018-10-21 05:17:33 +0200

    Adding slav background image to login screen

--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="container ">
-        <div class="row flex--center flex--middle vh100">
-            <div class="col-md-6">
-                <h2 class="pb20 text-center">Login to Kudos</h2>
-                <div class="flex flex--center">
-                    <a href="https://slack.com/oauth/authorize?scope=identity.basic identity.team identity.avatar&client_id=373180051559.455692683652&redirect_uri=http://localhost:3000/slack&state=user"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
+    <section style="background-image: url('/bg.png'); background-size:cover">
+        <div class="container">
+            <div class="row flex--center flex--middle vh100">
+                <div class="col-md-6">
+                    <!--<h2 class="pb20 text-center">Login to Kudos</h2>-->
+                    <div class="flex flex--center" style="transform: scale(1.2)">
+                        <a href="https://slack.com/oauth/authorize?scope=identity.basic identity.team identity.avatar&client_id=373180051559.455692683652&redirect_uri=http://localhost:3000/slack&state=user"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
+    </section>
 </template>
 
 <script>
